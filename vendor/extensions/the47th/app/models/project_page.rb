10419ba93c1919339b9bf6c3d5b1a6a413aed422
joel <joel.oliveira@gmail.com>
2011-10-07 11:51:25 -0400

    project and projects broken out into their respective scss files. markup updated for both. and project page factory cleaned up and documented with description params

--- vendor/extensions/the47th/app/models/project_page.rb
+++ vendor/extensions/the47th/app/models/project_page.rb
@@ -1,10 +1,11 @@
 class ProjectPage < Page
-  part 'Client Name',      :page_part_type => "StringPagePart"
-  part 'Project Title',    :page_part_type => "StringPagePart"
-  part 'Description',      :page_part_type => "StringPagePart"
+  part 'Client Name',      :page_part_type => "StringPagePart", :description => "The client name can be different from the project title."
+  part 'Type of Project',  :page_part_type => "StringPagePart", :description => "Website Redesign, Product design, Branding, Etc"
+  part 'Description',      :page_part_type => "StringPagePart", :description => "A short 1-paragraph description"
   part 'Roles',            :description    => "An unordered list of roles we played in this effort"
   part 'Thumbnail',        :page_part_type => "FilePagePart"
-  part 'Photos',           :description    => "An unordered list of images"
-  part 'Related Projects', :page_part_type => "StringPagePart", :description => "A comma-separated list of projects (their titles)"
+  part 'Photos',           :description    => "A collection of images (upload via assets)"
+  part 'Related Projects', :page_part_type => "StringPagePart", :description => "A comma-separated list of project titles (optional)"
   remove_part 'body'
+  remove_part 'extended'
 end
