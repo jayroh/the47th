087cb9936f062ee6085155a8d7c7959b5e60cba9
joel <joel.oliveira@gmail.com>
2011-10-09 15:09:29 -0400

    adding a description for the project thumbnail in project factory

--- vendor/extensions/the47th/app/models/project_page.rb
+++ vendor/extensions/the47th/app/models/project_page.rb
@@ -3,7 +3,7 @@ class ProjectPage < Page
   part 'Type of Project',  :page_part_type => "StringPagePart", :description => "Website Redesign, Product design, Branding, Etc"
   part 'Description',      :page_part_type => "StringPagePart", :description => "A short 1-paragraph description"
   part 'Roles',            :description    => "An unordered list of roles we played in this effort", :filter_id => 'Markdown'
-  part 'Thumbnail',        :page_part_type => "FilePagePart"
+  part 'Thumbnail',        :page_part_type => "FilePagePart",   :description => "An image sized at 210x210 pixels that will show up on the main Projects listing page"
   part 'Photos',           :description    => "A collection of images (upload via assets)", :filter_id => 'Markdown'
   part 'Related Projects', :description    => "A list of project titles and types (optional)", :content => "<ul>\n\t<li><a href=\"#\">\n\t\t<h2>Ring Boxing Club</h2>\n\t\t<p>Branding, Website, Business Cards</p>\n\t</a></li>\n</ul>"
   remove_part 'body'
